fa4678db2cd66edd71b5993b870fe1bda6d828ab
Yony BM <4yony4@gmail.com>
2022-09-30 13:00:48 +0200

    Primera descarga del Firebase Firestore

--- lib/src/home_views/HomeView.dart
+++ lib/src/home_views/HomeView.dart
@@ -1,19 +1,45 @@
 
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
+
     return _HomeViewState();
   }
 }
 
 class _HomeViewState extends State<HomeView>{
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getProfile();
+  }
+
+  void getProfile() async{
+    await db.collection("perfiles").doc("nLc7Fto81vgOOssLnSEIqDp0cHq2").get().then(
+          (DocumentSnapshot doc) {
+        final data = doc.data() as Map<String, dynamic>;
+        print("--------->>>>>>>>>>>>>>>>  "+data.toString());
+        // ...
+      },
+      onError: (e) => print("Error getting document: $e"),
+    );
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Chatdar'),
@@ -23,7 +49,7 @@ class _HomeViewState extends State<HomeView>{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("HOME VIEW")
+            Text("HOME VIEW BIENVENIDO: ")
           ],
         ),
       ),
