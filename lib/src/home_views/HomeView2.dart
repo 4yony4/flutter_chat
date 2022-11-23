07080d800d6486fad6e0cd8abddee76602ec5b50
Yony <4yony4@gmail.com>
2022-11-23 10:06:31 +0100

    Nueva accion de Github

--- lib/src/home_views/HomeView2.dart
+++ lib/src/home_views/HomeView2.dart
@@ -1,15 +1,19 @@
 
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/src/custom_views/RFInputText2.dart';
+import 'package:flutter_chat/src/json_objects/PilotoF1.dart';
 import 'package:flutter_chat/src/list_items/RoomItem.dart';
 import 'package:flutter_chat/src/singleton/DataHolder.dart';
 
 import '../fb_objects/Perfil.dart';
 import '../fb_objects/Perfil2.dart';
 import '../fb_objects/Room.dart';
+import 'package:http/http.dart' as http;
 
 class HomeView2 extends StatefulWidget{
   @override
@@ -31,6 +35,48 @@ class _HomeView2State extends State<HomeView2>{
     // TODO: implement initState
     super.initState();
     actualizarLista();
+    getPilotosF1();
+  }
+
+  void getPilotosF1() async{
+
+
+    int iAnio=2022;
+    final response = await http
+        .get(Uri.parse('http://ergast.com/api/f1/${iAnio}/drivers.json'));
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+
+      //print("DEBUG: --->>>>>>>>>   "+json.decode(response.body));
+      //print("DEBUG: --->>>>>>>>>   "+jsonDecode(response.body).toString());
+      Map<String, dynamic> json=jsonDecode(response.body);
+      //print("DEBUG: --->>>>>>>>>   "+json["MRData"].toString());
+      Map<String, dynamic> json2=json["MRData"];
+      //print("DEBUG: --->>>>>>>>>   "+json2["DriverTable"].toString());
+      Map<String, dynamic> json3=json2["DriverTable"];
+      //print("DEBUG: --->>>>>>>>>   "+json3["Drivers"].toStri ng());
+      List<dynamic> listaPilotos=json3["Drivers"];
+
+      List<dynamic> listaPilotos2=json["MRData"]["DriverTable"]["Drivers"];
+
+      List<PilotoF1> listaPilotosFinal=[];
+
+      for(int i=0;i<listaPilotos2.length;i++){
+        listaPilotosFinal.add(PilotoF1.fromJson(listaPilotos2[i]));
+      }
+
+      print("NOMBRE DEL PILOTO EN LA POSICION 10: "+listaPilotosFinal[10].givenName+"   "+listaPilotosFinal[10].familyName);
+
+      //print("DEBUG: --->>>>>>>>>   "+json["MRData"]["DriverTable"]["Drivers"].toString());
+
+      //return Album.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
   }
 
   /*
